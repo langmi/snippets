a36e7e8e70cbc1c6e71e6440b2eca97db5cd5336
langmi <daywalker2000@gmx.net>
2011-06-09 13:06:52 +0200

    initial check-in

---
+++ javasnippets/src/main/java/de/langmi/javasnippets/App.java
@@ -0,0 +1,13 @@
+package de.langmi.javasnippets;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
