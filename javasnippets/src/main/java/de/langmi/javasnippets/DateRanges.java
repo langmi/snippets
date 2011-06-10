c965867fb98e23910e3374b34931bef9b1c1095b
langmi <daywalker2000@gmx.net>
2011-06-10 17:16:26 +0200

    first work for daterange implementations

---
+++ javasnippets/src/main/java/de/langmi/javasnippets/DateRanges.java
@@ -0,0 +1,18 @@
+package de.langmi.javasnippets;
+
+/**
+ * Hello world!
+ *
+ */
+public class DateRanges {
+
+    /**
+     * Test if date ranges overlap.
+     *
+     * @return 
+     * @see http://c2.com/cgi/wiki?TestIfDateRangesOverlap
+     */
+    public boolean testIfDateRangesOverlap() {
+        return false;
+    }
+}
